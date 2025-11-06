a7be7563bd5d6185ea4e45d6becc6dc1715edba3
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:59:13 +0300

    User Contract

---
+++ Back/Application/Contracts/IUser.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.User.CreateRequest;
+using Application.DTOs.User.GetRequest;
+using Application.DTOs.User.GetRequestList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts
+{
+    public interface IUser
+    {
+        Task<CreateRequestResponse> CreateRequest(CreateRequestDTO createRequestDTO);
+        Task<GetRequestListResponse> GetRequestList(GetRequestListDTO getRequestListDTO);
+        Task<GetRequestResponse > GetResponseList(GetRequestListDTO getRequestListDTO);
+    }
+}
