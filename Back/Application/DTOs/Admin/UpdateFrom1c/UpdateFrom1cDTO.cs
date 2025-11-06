7cc0da116d6ade99353b1a7e86ce4a6632d89cd5
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:48:09 +0300

    UpdateFrom1c

---
+++ Back/Application/DTOs/Admin/UpdateFrom1c/UpdateFrom1cDTO.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Admin.UpdateFrom1c
+{
+    public class UpdateFrom1cDTO
+    {
+        [Required]
+        public Guid RequestID { get; set; } = default!;
+        [Required]
+        public OneCStatus Status { get; set; } = default!;
+        [Required]
+        public IFormFile? requestFile { get; set; } = default!;
+    }
+}
