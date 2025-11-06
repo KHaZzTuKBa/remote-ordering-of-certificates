053c5f83daf6ae7ec323b0bd2ddb4ff3c39bea65
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:48:50 +0300

    Models

---
+++ Back/Domain/Models/Request.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Request
+    {
+        public Guid Id { get; set; }
+        public RequestStatus requestStatus { get; set; }
+
+        public DateTime date {  get; set; }
+    }
+}
