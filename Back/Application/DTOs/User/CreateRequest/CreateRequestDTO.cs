5e9782b72df02e889497e8df099e5ba820d39a26
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:48:15 +0300

    CreateRequest

---
+++ Back/Application/DTOs/User/CreateRequest/CreateRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.User.CreateRequest
+{
+    public class CreateRequestDTO
+    {
+        [Required]
+        public int StudentId { get; set; } = default!;
+        [Required]
+        public string FullName { get; set; } = default!;
+        [Required]
+        public int Course { get; set; } = default!;
+    }
+}
