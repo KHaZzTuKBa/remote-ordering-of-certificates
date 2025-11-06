e564f7389712de7a65e635138c85ecbf9483f337
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:49:06 +0300

    Entity RequestInformation

---
+++ Back/Domain/Entities/RequestInformation.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+
+namespace Domain.Entities
+{
+    public class RequestInformation
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public RequestStatus requestStatus { get; set; } = default!;
+        public DateTime date { get; set; } = default!;
+        public IFormFile? requestFile { get; set; } = default!;
+    }
+}
