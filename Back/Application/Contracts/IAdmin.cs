242ecf1f010f65f782767d20a00597dac95b92f7
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:59:20 +0300

    Admin Contract

---
+++ Back/Application/Contracts/IAdmin.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.Admin.AdminGetRequest;
+using Application.DTOs.Admin.AdminGetRequestList;
+using Application.DTOs.Admin.SubmitTo1c;
+using Application.DTOs.Admin.UpdateFrom1c;
+
+namespace Application.Contracts
+{
+    public interface IAdmin
+    {
+        Task<AdminGetRequestResponse> AdminGetRequest(AdminGetRequestDTO adminGetRequestDTO);
+        Task<AdminGetRequestListResponse> AdminGetRequestList(AdminGetRequestListDTO adminGetRequestListDTO);
+        Task<SubmitTo1cResponse> SubmitTo1C(SubmitTo1cDTO submitTo1cDTO);
+        Task<UpdateFrom1cResponse> UpdateFrom1C (UpdateFrom1cDTO updateFrom1cDTO);
+    }
+}
