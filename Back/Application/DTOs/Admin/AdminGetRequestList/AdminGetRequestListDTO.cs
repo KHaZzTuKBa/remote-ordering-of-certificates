2f09d266e10616f4ee88175b7359336ca520fe1f
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:47:55 +0300

    AdminGetRequestList

---
+++ Back/Application/DTOs/Admin/AdminGetRequestList/AdminGetRequestListDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Admin.AdminGetRequestList
+{
+    public class AdminGetRequestListDTO
+    {
+        [Required]
+        public int StudentId { get; set; } = default!;
+    }
+}
---
+++ Back/Application/DTOs/Admin/AdminGetRequestList/AdminGetRequestListResponse.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Admin.AdminGetRequestList
+{
+    public record AdminGetRequestListResponse (List<Request> ListOfRequests);
+}
