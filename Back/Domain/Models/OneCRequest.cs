0a7b25252cab92491e77541cb2d6664d9a23bf0a
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 16:08:18 +0300

    Model OneCRequest

---
+++ Back/Domain/Models/OneCRequest.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class OneCRequest
+    {
+        public Guid Id { get; set; }
+        public IFormFile? requestFile { get; set; }
+    }
+}
