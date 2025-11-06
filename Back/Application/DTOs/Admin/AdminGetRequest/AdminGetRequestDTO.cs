f4c2096e7e1f4f6ba94d337b972c87301f62340a
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:47:44 +0300

    AdminGetRequest

---
+++ Back/Application/DTOs/Admin/AdminGetRequest/AdminGetRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Admin.AdminGetRequest
+{
+    public class AdminGetRequestDTO
+    {
+        [Required]
+        public Guid RequestID { get; set; } = default!;
+    }
+}
