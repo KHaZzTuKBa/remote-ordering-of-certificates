e252ce44ff23c00acc513b8a0233c06341c72ddc
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-11-06 13:48:32 +0300

    GetRequestList

---
+++ Back/Application/DTOs/User/GetRequestList/GetRequestListDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.User.GetRequestList
+{
+    public class GetRequestListDTO
+    {
+        [Required]
+        public int StudentId { get; set; } = default!;
+    }
+}
---
+++ Back/Application/DTOs/User/GetRequestList/GetRequestListResponse.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.User.GetRequestList
+{
+    public record GetRequestListResponse (List<Request> ListOfRequests);
+}
