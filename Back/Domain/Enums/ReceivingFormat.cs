5cc539825ebec82ff3f9d7b897c26ea103f7e52d
Nikolay <68915199+KHaZzTuKBa@users.noreply.github.com>
2025-12-04 16:40:18 +0300

    Update ReceivingFormat.cs

--- Back/Domain/Enums/ReceivingFormat.cs
+++ Back/Domain/Enums/ReceivingFormat.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace Domain.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))] 
     public enum ReceivingFormat
     {
         InPerson,
